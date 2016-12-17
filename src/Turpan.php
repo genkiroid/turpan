001f4cc3c19774ae076715cff71d46ccacddc2a6
genkiroid <gamurai@gmail.com>
2016-12-18 02:59:19 +0900

    Bump version

--- src/Turpan.php
+++ src/Turpan.php
@@ -8,7 +8,7 @@ use PhpParser\PrettyPrinter;
 
 class Turpan
 {
-    const VERSION = '0.2.8';
+    const VERSION = '0.3.0';
 
     const INCLUDE_STMT_PATTERN = '/^( *)(include_once|include|require_once|require)(\((?P<required_file_1>.*)\)| +(?P<required_file_2>.*));( *)$/';
 
