ea3e26ee9096a712f43118f6191b61729c1e1a0a
genkiroid <gamurai@gmail.com>
2023-01-28 13:01:13 +0900

    Change default branch name of repo for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,4 +7,4 @@ const TEST_REPO_URI = 'https://github.com/genkiroid/foo.git';
 const TEST_REPO_DIR = '/tmp/genkiroid/turpan/foo';
 
 exec('rm -rf ' . TEST_REPO_DIR);
-Admin::cloneBranchTo(TEST_REPO_DIR, TEST_REPO_URI, 'master', false);
+Admin::cloneBranchTo(TEST_REPO_DIR, TEST_REPO_URI, 'main', false);
